e83242c9813419c7f2ea467a848ab37a8a358e49
Nick Verheijen <verheijen.webdevelopment@gmail.com>
2020-06-22 23:30:08 +0200

    Renamed package

--- src/Providers/CurrencyServiceProvider.php
+++ src/Providers/CurrencyServiceProvider.php
@@ -1,31 +1,31 @@
 <?php
 
-namespace EZ\CurrencyConversion\Providers;
+namespace EZ\Currencies\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use EZ\CurrencyConversion\Services\CurrencyConversionService;
+use EZ\CurrencyConversion\Services\CurrencyService;
 
-class CurrencyConversionServiceProvider extends ServiceProvider
+class CurrenciesProvider extends ServiceProvider
 {
     public function register()
     {
         // Register the currency conversion service
-        $this->app->singleton("currency-conversion", function($app) {
-            return new CurrencyConversionService();
+        $this->app->singleton("currencies", function($app) {
+            return new CurrencyService();
         });
 
         // Setup loading of the migrations
         $this->loadMigrationsFrom(__DIR__."/../database/migrations");
 
         // Setup loading of the config file
-        $this->mergeConfigFrom(__DIR__."/../config/currency-conversion.php", "currency-conversion");
+        $this->mergeConfigFrom(__DIR__."/../config/currencies.php", "currencies");
     }
 
     public function boot()
     {
         // Setup publishing of the config file
         $this->publishes([
-            __DIR__."/../config/currency-conversion.php" => config_path("currency-conversion.php"),
+            __DIR__."/../config/currencies.php" => config_path("currencies.php"),
         ], "config");
 
         // Setup publishing of the migrations & seeds
@@ -33,7 +33,7 @@ class CurrencyConversionServiceProvider extends ServiceProvider
             __DIR__."/../database/migrations/create_currencies_table.php" => database_path('migrations/' . date('Y_m_d_His', time())  . '_create_currencies_table.php'),
             __DIR__."/../database/seeds/CurrencySeeder.php" => database_path("seeds/CurrencySeeder.php"),
         ], "database");
-        
+
 
     }
 }
---
+++ src/Services/CurrencyService.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace EZ\Currencies\Services;
+
+class CurrencyService
+{
+    
+}
